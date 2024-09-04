a229154f4069d6a2ae9cb9c780cb6254d542c114
Matheus Heimrath <129696259+Heimrath@users.noreply.github.com>
2024-09-04 19:55:11 -0300

    Add files via upload

---
+++ DOM-JJLM_Projeto.cpp
@@ -0,0 +1,11 @@
+//DOM-JJLM - Projeto Domino - Etapa 6
+//Grupo: JJLM
+
+#include "DOM-JJLM_Model.cpp"
+#include "DOM-JJLM_Controller.cpp"
+
+int main()
+{
+	criarPecas(); //Cria as pecas do jogo
+	executajogo(); //Embaralha as pecas e inicia o menu principal
+}
